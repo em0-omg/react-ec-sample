2a84ebfc0fcbd39795443c2e4d672670eaaff2d9
em0-omg <em0818.omg@gmail.com>
2021-02-16 22:12:44 +0900

    カートの中身がうまく更新されない不具合を修正

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -15,7 +15,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        carItems: [...state.cartItems, action.payload],
+        cartItems: [...state.cartItems, action.payload],
       };
     default:
       return state;
